55e9fbc393f91c4128c664be88f15b3aefc88dee
Gerard Keiser <gerardkeiser@gmail.com>
2022-06-17 14:10:42 -0500

    Improve styling

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tasks
+
+A very basic task manager
+
+## TODOs
+
+- [ ] Center add task form over task list
+- [ ] Better fonts
+- [ ] Uncomplete button
+- [ ] Fix border when task input is focused
+- [ ] Add more data for todos (completion time, tags, etc)
+- [ ] Fix display when task name overflows
+- [ ] Prevent empty task names
--- app.ts
+++ app.ts
@@ -5,88 +5,90 @@ const tasks = getTasks();
 refreshTasks();
 
 // Form for adding tasks
-document.querySelector('#add-task').addEventListener('submit', addTaskHandler);
+(document.querySelector("#add-task") as HTMLFormElement).addEventListener(
+  "submit",
+  addTaskHandler
+);
 
-function addTaskHandler(event: SubmitEvent) {
+function addTaskHandler(event: SubmitEvent): void {
+  event.preventDefault();
 
-    event.preventDefault();
+  const newTaskInput = document.querySelector("#task-name") as HTMLInputElement;
 
-    const newTaskInput = document.querySelector('#task-name') as HTMLInputElement;
+  const newTask: Task = {
+    name: newTaskInput.value,
+    completed: false,
+    id: Date.now(),
+  };
 
-    const newTask: Task = {
-        name: newTaskInput.value,
-        completed: false,
-        id: Date.now()
-    }
+  tasks.push(newTask);
 
-    tasks.push(newTask);
-
-    refreshTasks();
-
+  refreshTasks();
 }
 
 // For removing or completing tasks
-document.querySelector('#task-list').addEventListener('click', taskEditHandler);
+(document.querySelector("#task-list") as HTMLUListElement).addEventListener(
+  "click",
+  taskEditHandler
+);
 
-function taskEditHandler(event: PointerEvent) {
+function taskEditHandler(event: MouseEvent): void {
+  const target = event.target as HTMLElement;
+  const taskLi = target.parentElement as HTMLLIElement;
+  const taskIndex = tasks.findIndex((task) => task.id === +taskLi.id);
 
-    const target = event.target as HTMLElement;
-    const taskLi = target.parentElement;
-    const taskIndex = tasks.findIndex(task => task.id === +taskLi.id)
-
-    if (target.id === 'delete-btn') {
-        tasks.splice(taskIndex, 1);
-        refreshTasks();
-    } else if (target.id === 'complete-btn') {
-        tasks[taskIndex].completed = true;
-        refreshTasks();
-    }
+  if (target.classList.contains("delete-btn")) {
+    tasks.splice(taskIndex, 1);
+    refreshTasks();
+  } else if (target.classList.contains("complete-btn")) {
+    tasks[taskIndex].completed = true;
+    refreshTasks();
+  }
 }
 
 // Helpers
 interface Task {
-    name: string;
-    completed: boolean;
-    id: number;
+  name: string;
+  completed: boolean;
+  id: number;
 }
 
 function getTasks(): Task[] {
-    if (!localStorage.tasks) {
-        return [];
-    } else {
-        return JSON.parse(localStorage.tasks)
-    }
+  if (!localStorage.tasks) {
+    return [];
+  } else {
+    return JSON.parse(localStorage.tasks);
+  }
 }
 
-function refreshTasks() {
+function refreshTasks(): void {
+  localStorage.tasks = JSON.stringify(tasks);
 
-    localStorage.tasks = JSON.stringify(tasks);
+  const taskList = document.querySelector("#task-list") as HTMLUListElement;
+  taskList.replaceChildren("");
 
-    const taskList = document.querySelector('#task-list');
-    taskList.replaceChildren('');
+  tasks.forEach((task): void => {
+    // Make elements
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "X";
+    deleteButton.classList.add("delete-btn");
 
-    tasks.forEach(function(task) {
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "Complete";
+    completeButton.classList.add("complete-btn");
 
-        // Make elements
-        const deleteButton = document.createElement('button');
-        deleteButton.innerText = "Delete";
-        deleteButton.id = "delete-btn";
+    const taskLi = document.createElement("li");
+    taskLi.innerText = task.name;
+    taskLi.id = String(task.id);
 
-        const completeButton = document.createElement('button');
-        completeButton.innerText = "Complete";
-        completeButton.id = "complete-btn";
-
-        const taskLi = document.createElement('li');
-        taskLi.innerText = task.name;
-        taskLi.id = String(task.id);
-
-        // Include elements
-        taskLi.append(deleteButton);
-        if (task.completed === false) {
-            taskLi.append(completeButton);
-        } else {
-            taskLi.classList.add('completed');
-        }
-        taskList.append(taskLi);
-    })
+    // Include elements
+    taskLi.append(deleteButton);
+    if (task.completed === false) {
+      taskLi.append(completeButton);
+      taskList.prepend(taskLi);
+    } else {
+      taskLi.classList.add("completed");
+      taskList.append(taskLi);
+    }
+  });
 }
